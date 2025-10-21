45b5a9a808256d1949808a887696a14a32faca26
ankursasmal <ankursasmal2017@gmail.com>
2025-10-21 23:20:56 +0530

    add origin in frontend

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 app.use(cookieParser());
 app.use(cors({
-  origin: "http://localhost:5173", 
+  origin: ["http://localhost:5173","https://localhost:5173","*"  ], 
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
