2236b8c7c5a45261fe242012961c7cf2e9a74a2e
ankursasmal <ankursasmal2017@gmail.com>
2025-10-30 19:20:48 +0530

    add populate option

--- backend/controlers/cart/getAllCart.js
+++ backend/controlers/cart/getAllCart.js
@@ -3,7 +3,7 @@ let cartModel = require('../../models/cartModel');
 const getAllCart = async (req, res) => {
     try {
         let userId = req.user._id;
-        let cart = await cartModel.find({userId});
+        let cart = await cartModel.find({userId}).populate('productId', 'name price image description catagory discount stock limit ');
         res.json({ message: 'Cart fetched successfully', status: 200, data: cart, success: true, error: false });
     }
     catch (e) {
